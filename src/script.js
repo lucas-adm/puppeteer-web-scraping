ea19d6d500ca9f63a3bf5d7e68464ad8fa5dacc9
Lucas Aguiar <vintesetelucas@gmail.com>
2024-01-30 21:51:57 -0300

    first commit

---
+++ src/script.js
@@ -0,0 +1,143 @@
+const gotoContainer = document.getElementById('goto')
+const responseContainer = document.getElementById('container')
+
+async function getResults(url) {
+    responseContainer.innerHTML = '';
+
+    const response = await fetch('http://localhost:3000/content', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ url })
+    })
+
+    const contents = await response.json();
+    for (const key in contents) {
+        if (contents.hasOwnProperty(key)) {
+            const content = contents[key]
+            const chapter = `
+            <div class="result">
+                <span>${content.h2[0]}</span>
+                <button onclick="getChapters('https://angular-blog-2mq9.onrender.com')">${content.p[0]}</button>
+            </div>
+                            `
+
+            responseContainer.innerHTML += chapter
+        }
+    }
+}
+
+async function getChapters(url) {
+    responseContainer.innerHTML = '';
+
+    const response = await fetch('http://localhost:3000/titles', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ url })
+    })
+    const titles = await response.json();
+    for (const key in titles) {
+        if (titles.hasOwnProperty(key)) {
+            const title = titles[key]
+            const h1 = `
+            <div class="chapter">
+                <button onclick="getImgs('https://angular-blog-2mq9.onrender.com')">${title}</button>
+            </div>
+                        `
+            responseContainer.innerHTML += h1
+        }
+    }
+}
+
+async function getImgs(url) {
+    responseContainer.innerHTML = '';
+
+    const response = await fetch('http://localhost:3000/imgs', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ url })
+    })
+    // const urls = await response.text();
+    const urls = await response.json();
+    for (const key in urls) {
+        if (urls.hasOwnProperty(key)) {
+            const url = urls[key]
+            const img = `
+                        <div class="content">
+                            <img src="${url}" alt="">
+                        </div>
+                        `
+            responseContainer.innerHTML += img
+        }
+    }
+}
+
+//Crias os parâmetros
+function createParams(parent) {
+    const params = `
+    <div class="params">
+        <button parent="${parent}" onclick="createForm(${parent}, 'results')">Results</button>
+        <button parent="${parent}" onclick="createForm(${parent},'chapters')">Chapters</button>
+        <button parent="${parent}" onclick="createForm(${parent}, 'contents')">Content</button>
+    </div>
+    `
+    gotoContainer.innerHTML = params
+}
+
+//Cria o path
+function createForm(parent, param) {
+    lastForm = gotoContainer.querySelector('#path:last-child')
+    if (lastForm) {
+        lastForm.remove()
+    }
+
+    const form = `
+    <div id="path" class="path" action="javascript:void(0)">
+        <form action="">
+            <label for="path">${parent} - ${param}</label>
+            <input parent="${parent}" param="${param}" type="text" name="path" id="input" value="">
+        </form>
+    </div>
+    `
+
+    gotoContainer.innerHTML += form
+
+    //Onde fica o Form
+    const goto = gotoContainer.querySelector('#path');
+
+    goto.addEventListener('submit', (event) => {
+
+        event.preventDefault();
+
+        const input = goto.querySelector('#input')
+        const parent = input.getAttribute('parent');
+        const param = input.getAttribute('param');
+        const url = input.value
+
+        async function scraping() {
+            if (!url) {
+                return;
+            }
+
+            //Condição 1
+            if (parent === '1' && param === 'results') {
+                getResults(url)
+            }
+
+            //Condição 2
+            if (parent === '1' && param === 'chapters') {
+                getChapters(url)
+            }
+
+            //Condição 3
+            if (parent === '1' && param === 'contents') {
+                getImgs(url)
+            }
+        } scraping()
+    })
+}
---
+++ src/server.js
@@ -0,0 +1,173 @@
+const express = require('express');
+const puppeteer = require('puppeteer');
+const cors = require('cors');
+
+const app = express();
+
+// Configurações
+const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || 'localhost'
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://${HOST}:${PORT}}`);
+});
+
+
+
+//Results
+async function getTitles(url) {
+    const browser = await puppeteer.launch({
+        headless: 'new', // Usar o novo modo Headless
+    });
+    const page = await browser.newPage();
+
+    // Navegar até a página desejada
+    await page.goto(url, { waitUntil: 'domcontentloaded' });
+
+    // Capturar o valor text content das tags <a> que estão dentro de <h1>
+    const titles = await page.evaluate(() => {
+        const titlesArray = [];
+        const h1Elements = document.querySelectorAll('h1');
+
+        h1Elements.forEach((h1) => {
+            const aElements = h1.querySelectorAll('a');
+            aElements.forEach((a) => {
+                titlesArray.push(a.textContent.trim());
+            });
+        });
+
+        return titlesArray;
+    });
+
+    await browser.close();
+
+    return titles;
+}
+
+app.post('/titles', async (req, res) => {
+    const url = req.body.url;
+
+    if (!url) {
+        return res.status(400).send('URL não fornecida.');
+    }
+    try {
+        const titles = await getTitles(url);
+        // res.send(urls); .send faz o envio de strings
+        res.json(titles); // envia um array
+    } catch (error) {
+        console.error('Erro ao raspar Img:', error);
+        res.status(500).send('Erro ao raspar os titles.');
+    }
+})
+
+
+
+//Content
+async function getContent(url) {
+    const browser = await puppeteer.launch({
+        headless: 'new', // Usar o novo modo Headless
+    });
+    const page = await browser.newPage();
+
+    // Navegar até a página desejada
+    await page.goto(url, { waitUntil: 'domcontentloaded' });
+
+    // Aguardar 3 segundos para garantir que as informações sejam carregadas
+    await new Promise(resolve => setTimeout(resolve, 3000));
+
+    // Capturar o text content de cada h2, cada p e cada href dentro da div com classe .movie-card
+    const content = await page.evaluate(() => {
+        const contentArray = [];
+        const movieCardElements = document.querySelectorAll('.movie-card');
+
+        movieCardElements.forEach((movieCard) => {
+            const h2Elements = movieCard.querySelectorAll('h2');
+            const pElements = movieCard.querySelectorAll('p');
+            const aElements = movieCard.querySelectorAll('a');
+
+            const contentObject = {
+                h2: [],
+                p: [],
+                href: [],
+            };
+
+            h2Elements.forEach((h2) => {
+                contentObject.h2.push(h2.textContent.trim());
+            });
+
+            pElements.forEach((p) => {
+                contentObject.p.push(p.textContent.trim());
+            });
+
+            aElements.forEach((a) => {
+                contentObject.href.push(a.getAttribute('href'));
+            });
+
+            contentArray.push(contentObject);
+        });
+
+        return contentArray;
+    });
+
+    await browser.close();
+
+    return content;
+}
+
+app.post('/content', async (req, res) => {
+    const url = req.body.url;
+
+    if (!url) {
+        return res.status(400).send('URL não fornecida.');
+    }
+    try {
+        const content = await getContent(url);
+        res.json(content); // envia um array
+    } catch (error) {
+        console.error('Erro ao raspar Img:', error);
+        res.status(500).send('Erro ao raspar o conteúdo.');
+    }
+})
+
+
+
+//Imgs
+async function imgsUrls(url) {
+
+    const browser = await puppeteer.launch({
+        headless: 'new', // Usar o novo modo Headless
+    });
+    const page = await browser.newPage();
+
+    // Navegar até a página desejada
+    await page.goto(url, { waitUntil: 'domcontentloaded' });
+
+    // Extrair URLs de imagens na página
+    const urls = await page.evaluate(() => {
+        const imagens = Array.from(document.querySelectorAll('img'));
+        return imagens.map(img => img.src);
+    });
+
+    await browser.close();
+
+    return urls;
+}
+
+app.post('/imgs', async (req, res) => {
+    const url = req.body.url;
+
+    if (!url) {
+        return res.status(400).send('URL não fornecida.');
+    }
+    try {
+        const urls = await imgsUrls(url);
+        // res.send(urls); .send faz o envio de strings
+        res.json(urls); // envia um array
+    } catch (error) {
+        console.error('Erro ao raspar Img:', error);
+        res.status(500).send('Erro ao raspar Img.');
+    }
+})
